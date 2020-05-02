a646c09784454ed31365181d22b84929db5c5a1e
mredition <59235302+mredition@users.noreply.github.com>
2020-05-02 15:51:05 +0200

    Update group.py

--- userbot/plugins/group.py
+++ userbot/plugins/group.py
@@ -6,4 +6,4 @@ async def join(e):
 
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
 
-        await e.edit("This is my community.\n\n[Channel](http://t.me/giveaways_24hrs)\n\n[Chat Group](http://t.me/giveaways24hrsdiscuss)\n\n[UserBot Tutorial](https://t.me/TeleBotHelp)")
+        await e.edit("This is my community.\n\n[Channel](http://t.me/freeitemsinside)\n\n[Chat Group](http://t.me/freenetedition)\n\n[UserBot Tutorial]")
